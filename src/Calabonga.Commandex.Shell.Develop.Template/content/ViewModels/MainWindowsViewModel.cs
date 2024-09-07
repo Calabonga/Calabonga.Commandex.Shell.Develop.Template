d6f637e2e39710d259487c2ecdfe8f293f086565
Sergei Calabonga <dev@calabonga.net>
2024-09-07 14:03:24 +0700

    engine nuget version updates

--- src/Calabonga.Commandex.Shell.Develop.Template/content/ViewModels/MainWindowsViewModel.cs
+++ src/Calabonga.Commandex.Shell.Develop.Template/content/ViewModels/MainWindowsViewModel.cs
@@ -16,7 +16,7 @@ public partial class MainWindowsViewModel : ViewModelBase
     public MainWindowsViewModel(IDialogService dialogService, IAppSettings settings)
     {
         Title = $"Commandex Shell Emulator for Easy developing ({settings.CommandsPath})";
-        Version = "1.0.0-rc.7";
+        Version = "1.0.0-rc.9";
         _dialogService = dialogService;
     }
 
